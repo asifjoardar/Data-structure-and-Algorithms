5e6b3be2dfbcbef85aaa27ab50fc6852ceecac89
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-09-10 19:02:46 +0600

    Create segmenttree.cpp

---
+++ Segment Tree/segmenttree.cpp
@@ -0,0 +1,93 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI 2*acos(0.0)
+#define PII pair<ll, ll>
+#define pii pair<int, int>
+#define what_is(x) cerr<<#x<<" is "<<x<<endl;
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+#define mx 100001
+int arr[mx];
+int tree[mx * 3];
+void init(int node, int b, int e)
+{
+    if (b == e) {
+        tree[node] = arr[b];
+        return;
+    }
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (b + e) / 2;
+    init(Left, b, mid);
+    init(Right, mid + 1, e);
+    tree[node] = tree[Left] + tree[Right];
+}
+int query(int node, int b, int e, int i, int j)
+{
+    if (i > e || j < b)
+        return 0;
+    if (b >= i && e <= j)
+        return tree[node];
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (b + e) / 2;
+    int p1 = query(Left, b, mid, i, j);
+    int p2 = query(Right, mid + 1, e, i, j);
+    return p1 + p2;
+}
+void update(int node, int b, int e, int i, int newvalue)
+{
+    if (i > e || i < b)
+        return;
+    if (b >= i && e <= i) {
+        tree[node] = newvalue;
+        return;
+    }
+    int Left = node * 2;
+    int Right = node * 2 + 1;
+    int mid = (b + e) / 2;
+    update(Left, b, mid, i, newvalue);
+    update(Right, mid + 1, e, i, newvalue);
+    tree[node] = tree[Left] + tree[Right];
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+        cin>> arr[i];
+    init(1, 1, n);
+    update(1, 1, n, 2, 0);
+    cout << query(1, 1, n, 1, 3) << endl;
+    update(1, 1, n, 2, 2);
+    cout << query(1, 1, n, 2, 2) << endl;
+    return 0;
+}
