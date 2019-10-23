e18ffc0247d24485dc873fdbd4c54dae3767880a
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-10-23 20:17:18 +0600

    Create bit.cpp
    data structure

---
+++ binary indexed tree/bit.cpp
@@ -0,0 +1,72 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define white 1
+#define gray 2
+#define black 3
+#define PII pair<ll, ll> 
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+#define mx 10000
+ll ar[mx],tree[mx];
+
+ll read(ll idx)
+{
+    ll sum=0;
+    while(idx>0)
+    {
+        sum+=tree[idx];
+        idx-=(idx & -idx);
+    }
+    return sum;
+}
+
+void update(ll idx,ll val,ll n)
+{
+    while(idx<=n)
+    {
+        tree[idx]+=val;
+        idx+=(idx & -idx);
+    }
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    ll n,i;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cin>>ar[i];
+        update(i,ar[i],n);
+    }
+    cout<<read(5)<<endl;
+    return 0;
+}
