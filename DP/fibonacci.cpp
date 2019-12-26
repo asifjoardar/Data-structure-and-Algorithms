9ca2182ae5fd9a969c328578d756f82c1ca851d8
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-12-26 21:34:15 +0600

    dp

---
+++ DP/fibonacci.cpp
@@ -0,0 +1,53 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll, ll> 
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+int dp[500];
+
+int fibo(int n)
+{
+    if(n<=1)
+        return n;
+    if(dp[n]>-1)
+        return dp[n];
+    return dp[n]=fibo(n-1)+fibo(n-2);
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    ll i;
+    for(i=0;i<500;i++)
+        dp[i]=-1;
+    cout<<fibo(5)<<endl;
+    return 0;
+}
