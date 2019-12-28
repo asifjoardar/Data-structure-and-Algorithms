a7acccb39993c892ff1251fb56e24a6f25e860ae
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2019-12-28 20:21:02 +0600

    Create subset1.cpp

---
+++ bitmask/subset1.cpp
@@ -0,0 +1,55 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll, ll> 
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+void bitmask(string s,ll n)
+{
+    ll ss=pow(2,n),i,j;
+    for(i=1;i<ss;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i&(1<<j))
+                cout<<s[j];
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    fio();
+    string s;
+    cin>>s;
+    bitmask(s,s.size());
+    return 0;
+}
