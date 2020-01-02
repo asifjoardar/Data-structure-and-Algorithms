b5091a5c6726b7846c4352078b56b5c64abe0113
MD. Asif Joardar <35629053+asifjoardar@users.noreply.github.com>
2020-01-02 11:34:23 +0600

    LIS

---
+++ backtracking algo/lis.cpp
@@ -0,0 +1,54 @@
+/** BiSmIlLaHiR rAhMaNiR rAhIm **\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||||||||  ||||||||  ||||||||   *$*
+*$*    ||    ||  ||           ||     ||         *$*
+*$*    ||||||||  ||||||||     ||     ||||||     *$*
+*$*    ||    ||        ||     ||     ||         *$*
+*$*    ||    ||  ||||||||  ||||||||  ||         *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+            \**  DIIT(17th batch) **/
+
+#include<bits/stdc++.h>
+#define ll long long int
+#define ul unsigned long long int
+#define pf printf
+#define sf scanf
+#define endl "\n"
+#define pb push_back
+#define makep make_pair
+#define MOD 1000000007
+#define PI acos(-1.0)
+#define PII pair<ll, ll> 
+#define SIZE 1000005
+#define fio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+ll fx[]={1,0,-1,0,-1,-1,1,1};
+ll fy[]={0,1,0,-1,-1,1,-1,1};
+
+ll n;
+ll a[100];
+ll ss(ll i,ll prev)
+{
+    if(i>=n) return 0;
+    ll ans=ss(i+1,prev);
+    ll ans1=0;
+    if(a[i]>prev)
+        ans1=ss(i,a[i])+1;
+    return max(ans,ans1);
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ll i;
+    cin>>n;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    cout<<ss(0,INT_MIN)<<endl;
+    return 0;
+}
